82371837e355f6d9d0362da317010cb772ebaac6
rvua <rvanore26@gmail.com>
2022-04-27 20:40:43 -0500

    finished login/registration functionality

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,3 +1,4 @@
+import re
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
@@ -57,8 +58,20 @@ def login():
 # ===========================
 @app.route("/dashboard")
 def dashboard():
+    if "user_id" not in session:
+        flash("Please register or login")
+        return redirect("/")
     data = {
         "user_id" : session['user_id']
     }
-    User.get_by_id(data)
-    return render_template("dashboard.html")
+    logged_user = User.get_by_id(data)
+    return render_template("dashboard.html", user = logged_user)
+
+# ==========================
+# Logout Route
+# ==========================
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("Successfully logged out")
+    return redirect("/")
