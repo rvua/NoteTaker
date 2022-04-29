b01419d6d5729008d3e7d634ca7261fe124cdb65
rvua <rvanore26@gmail.com>
2022-04-29 12:20:05 -0500

    note model, controller, new_note.html

--- flask_app/controllers/note_controller.py
+++ flask_app/controllers/note_controller.py
@@ -7,3 +7,12 @@ from flask import render_template, redirect, request, session, flash
 @app.route("/new_note")
 def new_note():
     return render_template("new_note.html")
+
+@app.route("/create_note")
+def create_note():
+    #1. Validate info about note
+    if not Notde.validate_note(request.form):
+        return redirect("/new_note")
+    #2. Save note to db w/ logged in user's info attached
+    #3. Send us where we need to go.
+    return redirect("/dashboard") 
---
+++ flask_app/models/note.py
@@ -0,0 +1,14 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash 
+
+class Note:
+    def __init__(self, data):
+        self.id = data['id']
+        self.title = data['title']
+        self.description = data['description']
+        self.note = data['note']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.user_id = data['user_id']
+
+    
